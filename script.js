3a6636bfc58e4427cf0806a406fff8299c7af642
Axel <axelarep@gmail.com>
2022-03-18 21:11:08 -0600

    Functions of operators have been added

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(val1, val2){
+    return val1 + val2;
+}
+function subtract(val1, val2){
+    return val1 - val2;
+}
+function multiply(val1, val2){
+    return val1 * val2;
+}
+function divide(val1, val2){
+    return val1 / val2;
+}
+
+function operate(operator, val1, val2){
+
+    }
+
+
+
+// operate([0], 3,5)
