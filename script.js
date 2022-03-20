f36aa7ba2af25d447f915629839aad63b62c21c1
Axel <axelarep@gmail.com>
2022-03-20 12:15:49 -0600

    Calculator is working only with the first 2 input values

--- script.js
+++ script.js
@@ -7,39 +7,39 @@ containerDisplay.innerHTML = displayArray;
 
 // Event listener for each number of the calculator
 
-// Key7
-let key7 = document.querySelector('#key7');
-key7.addEventListener('click', press7 = function(){
+// Key1
+let key1 = document.querySelector('#key1');
+key1.addEventListener('click', press1 = function(){
 
     if(calculateArray == ''){
-        displayArray.push(7)
+        displayArray.push(1)
         containerDisplay.innerHTML = displayArray.join('');
     }else{
-        numberArray.push(7)
+        numberArray.push(1)
         containerDisplay.innerHTML = numberArray.join('');
     }
 })
 
-// Key8
-let key8 = document.querySelector('#key8');
-key8.addEventListener('click', press8 = function(){
+// Key2
+let key2 = document.querySelector('#key2');
+key2.addEventListener('click', press2 = function(){
     if(calculateArray == ''){
-        displayArray.push(8)
+        displayArray.push(2)
         containerDisplay.innerHTML = displayArray.join('');
     }else{
-        numberArray.push(8)
+        numberArray.push(2)
         containerDisplay.innerHTML = numberArray.join('');
     }
 })
 
-// Key9
-let key9 = document.querySelector('#key9');
-key9.addEventListener('click', press9 = function(){
+// Key3
+let key3 = document.querySelector('#key3');
+key3.addEventListener('click', press3 = function(){
     if(calculateArray == ''){
-        displayArray.push(9)
+        displayArray.push(3)
         containerDisplay.innerHTML = displayArray.join('');
     }else{
-        numberArray.push(9)
+        numberArray.push(3)
         containerDisplay.innerHTML = numberArray.join('');
     }
 })
@@ -80,14 +80,26 @@ key6.addEventListener('click', press6 = function(){
     }
 })
 
-// Key9
-let key9 = document.querySelector('#key9');
-key9.addEventListener('click', press9 = function(){
+// Key7
+let key7 = document.querySelector('#key7');
+key7.addEventListener('click', press7 = function(){
     if(calculateArray == ''){
-        displayArray.push(9)
+        displayArray.push(7)
         containerDisplay.innerHTML = displayArray.join('');
     }else{
-        numberArray.push(9)
+        numberArray.push(7)
+        containerDisplay.innerHTML = numberArray.join('');
+    }
+})
+
+// Key8
+let key8 = document.querySelector('#key8');
+key8.addEventListener('click', press8 = function(){
+    if(calculateArray == ''){
+        displayArray.push(8)
+        containerDisplay.innerHTML = displayArray.join('');
+    }else{
+        numberArray.push(8)
         containerDisplay.innerHTML = numberArray.join('');
     }
 })
@@ -104,28 +116,43 @@ key9.addEventListener('click', press9 = function(){
     }
 })
 
-// Key9
-let key9 = document.querySelector('#key9');
-key9.addEventListener('click', press9 = function(){
+// Key0
+let key0 = document.querySelector('#key0');
+key0.addEventListener('click', press0 = function(){
     if(calculateArray == ''){
-        displayArray.push(9)
+        displayArray.push(0)
         containerDisplay.innerHTML = displayArray.join('');
     }else{
-        numberArray.push(9)
+        numberArray.push(0)
         containerDisplay.innerHTML = numberArray.join('');
     }
 })
 
-// Key9
-let key9 = document.querySelector('#key9');
-key9.addEventListener('click', press9 = function(){
-    if(calculateArray == ''){
-        displayArray.push(9)
-        containerDisplay.innerHTML = displayArray.join('');
-    }else{
-        numberArray.push(9)
-        containerDisplay.innerHTML = numberArray.join('');
-    }
+// Key "+"
+let keyAdd = document.querySelector('#key-add');
+keyAdd.addEventListener('click', function pressAdd(){
+     displayArray = displayArray.join('')
+     displayArray = Number(displayArray)
+     calculateArray.push(displayArray)
+     keyAdd.classList.add('isClicked')
+})
+
+// Key "/"
+let keySubtract = document.querySelector('#key-subtract');
+keySubtract.addEventListener('click', function pressSubtract(){
+    displayArray = displayArray.join('')
+    displayArray = Number(displayArray)
+    calculateArray.push(displayArray)
+    keySubtract.classList.add('isClicked')
+})
+
+// Key "X"
+let keyMultiply = document.querySelector('#key-multiply');
+keyMultiply.addEventListener('click', function pressMultiply(){
+    displayArray = displayArray.join('')
+    displayArray = Number(displayArray)
+    calculateArray.push(displayArray)
+    keyMultiply.classList.add('isClicked')
 })
 
 // Key "/"
@@ -134,6 +161,7 @@ keyDivide.addEventListener('click', function pressDivide(){
     displayArray = displayArray.join('')
     displayArray = Number(displayArray)
     calculateArray.push(displayArray)
+    keyDivide.classList.add('isClicked')
 })
 
 // Key "="
@@ -142,8 +170,18 @@ keyEqual.addEventListener('click', function pressEqual(){
     numberArray = numberArray.join('')
     numberArray = Number(numberArray)
     calculateArray = Number(calculateArray)
-    // console.log(calculateArray / numberArray)
-    containerDisplay.innerHTML = calculateArray / numberArray;
+
+    if (keyDivide.classList.contains('isClicked')){ // Check if Divide Key was pressed in order to divide the 2 values
+        containerDisplay.innerHTML = calculateArray / numberArray;
+
+    } else if(keyMultiply.classList.contains('isClicked')){ // Check if Multiply Key was pressed in order to multiply the 2 values
+        containerDisplay.innerHTML = calculateArray * numberArray;
+
+    } else if(keyAdd.classList.contains('isClicked')){ // Check if Add Key was pressed in order to sum the 2 values
+        containerDisplay.innerHTML = calculateArray + numberArray;
+    } else if(keySubtract.classList.contains('isClicked')){ // Check if Subtract Key was pressed in order to subtract the 2 values
+        containerDisplay.innerHTML = calculateArray - numberArray;
+    }
 })
 
 
